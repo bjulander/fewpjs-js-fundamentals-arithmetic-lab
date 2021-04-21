ffef46c938e9f33325f86afc917d029fa79316e0
Blake Julander <13lakejulander@gmail.com>
2021-04-21 15:06:58 -0700

    commit tree c9aeefdd153a1895364bcfe8aaf9916570a98119

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
 
 // Write your code here
+let num1 = 2
+let num2 = 31
+
+const num3 = 16;
+const num4 = 6;
+
+let multiply = num1 * num2
+
+let random = Math.ceil(Math.random())
+
+let mod = num3 % num4
+
+let max = Math.max(1, 20)
